6b1aea5166e23de5271848492ebcc13aa2440e9f
2dLynxGames <2dLynxGames@gmail.com>
2020-07-29 04:35:20 -0400

    Added player controller to level manager Currently used for enemies locating the player

--- Fenris-Manor/Assets/Scripts/LevelManager.cs
+++ Fenris-Manor/Assets/Scripts/LevelManager.cs
@@ -4,10 +4,12 @@ using UnityEngine;
 
 public class LevelManager : MonoBehaviour
 {
+    public PlayerController playerController;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        playerController = FindObjectOfType<PlayerController>();
     }
 
     // Update is called once per frame
