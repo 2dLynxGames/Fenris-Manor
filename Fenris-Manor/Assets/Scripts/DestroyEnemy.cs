a520b15fc5196022c3e206e3a922adfc0a1bbdac
2dLynxGames <2dLynxGames@gmail.com>
2020-07-30 00:36:31 -0400

    Initial commit of destroy enemy

---
+++ Fenris-Manor/Assets/Scripts/DestroyEnemy.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyEnemy : MonoBehaviour
+{
+    //public GameObject deathAnimation;
+
+    private PlayerController playerController;
+
+    private int damageToDo;
+
+
+    private
+    // Start is called before the first frame update
+    void Awake()
+    {
+        playerController = transform.GetComponentInParent<PlayerController>();
+    }
+
+    void Start() {
+        damageToDo = playerController.GetWhipDamage();
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        Debug.Log(other.tag);
+        if (other.tag == "Enemy") {
+            other.GetComponent<iEnemyController>().TakeDamage(damageToDo);
+        }
+    }
+
+
+}
