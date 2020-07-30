19b77880b9890f5dd7f068bc66e4d4498318381b
2dLynxGames <2dLynxGames@gmail.com>
2020-07-30 00:38:17 -0400

    Initial commit of spawn enemy

---
+++ Fenris-Manor/Assets/Scripts/EnemyScripts/SpawnEnemy.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnEnemy : MonoBehaviour
+{
+
+    public GameObject enemyToSpawn;
+    public bool isVisible = false;
+
+    public bool enemyIsAlive = false;
+    private bool canSpawnEnemy = true;
+
+    void OnBecameVisible()
+    {
+        isVisible = true;
+        Debug.Log("Spawning Enemy");
+        if (canSpawnEnemy) {
+            Instantiate(enemyToSpawn, transform);
+            enemyIsAlive = true;
+            canSpawnEnemy = false;
+        }
+    }
+
+    void OnBecameInvisible()
+    {
+        if (!enemyIsAlive) {
+            canSpawnEnemy = true;
+            isVisible = false;
+        }
+    }
+
+    void Update()
+    {
+        if (isVisible && canSpawnEnemy && !enemyIsAlive) {
+            Instantiate(enemyToSpawn, transform);
+            enemyIsAlive = true;
+            canSpawnEnemy = false;
+        }
+    }
+}
