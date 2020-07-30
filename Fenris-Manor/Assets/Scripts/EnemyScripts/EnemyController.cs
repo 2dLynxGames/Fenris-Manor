6574d8e62a3174d6a23acef5459e69d51ba981b1
2dLynxGames <2dLynxGames@gmail.com>
2020-07-30 00:37:23 -0400

    initial commit of iEnemyController

---
+++ Fenris-Manor/Assets/Scripts/EnemyScripts/EnemyController.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+interface iEnemyController
+{
+    void TakeDamage(int damageToTake);
+    void DealDamage(int damageToDo);
+    bool GetIsDead();
+    void SetIsDead(bool dead);
+}
