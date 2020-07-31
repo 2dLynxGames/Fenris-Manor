8c1b35077c33e8acce25e760f0520f25480d5138
2dLynxGames <2dLynxGames@gmail.com>
2020-07-31 05:40:49 -0400

    Removed Debug line

--- Fenris-Manor/Assets/Scripts/DamagePlayer.cs
+++ Fenris-Manor/Assets/Scripts/DamagePlayer.cs
@@ -19,7 +19,6 @@ public class DamagePlayer : MonoBehaviour
 
     void OnTriggerEnter2D(Collider2D other)
     {
-        Debug.Log(other.tag);
         if (other.tag == "Player") {
             this.GetComponent<IEnemyController>().DealDamage();
         }
