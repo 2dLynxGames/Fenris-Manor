386fa32a9f53ec346e3ebb8aec3cdec67cc65ccf
2dLynxGames <2dLynxGames@gmail.com>
2020-07-31 21:53:32 -0400

    Added a function to stop enemies from spawning Useful for doors and cut scenes

--- Fenris-Manor/Assets/Scripts/LevelManager.cs
+++ Fenris-Manor/Assets/Scripts/LevelManager.cs
@@ -5,16 +5,24 @@ using UnityEngine;
 public class LevelManager : MonoBehaviour
 {
     public PlayerController playerController;
+    public SpawnEnemy[] enemies;
 
     // Start is called before the first frame update
     void Awake()
     {
         playerController = GameObject.Find("Player").GetComponent<PlayerController>();
+        enemies = GameObject.FindObjectsOfType<SpawnEnemy>();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public void ToggleEnemies(bool state) {
+        foreach (var enemy in enemies) {
+            enemy.gameObject.SetActive(state);
+        }
     }
 }
