95321b12f6422f2bcf717eb48f98c5f26cd36d52
2dLynxGames <2dLynxGames@gmail.com>
2020-07-31 21:51:30 -0400

    Added Coroutine to set inDoor and stop spawns

--- Fenris-Manor/Assets/Scripts/DoorTrigger.cs
+++ Fenris-Manor/Assets/Scripts/DoorTrigger.cs
@@ -6,17 +6,22 @@ public class DoorTrigger : MonoBehaviour {
 
     public GameObject roomSpawnPoint;
     public CameraController cameraController;
+    public float timeToWait = 1;
 
     protected PlayerController playerController;
     protected PlayerPlatformerController platformerController;
+    protected LevelManager levelManager;
     
     public GameObject newRoomMinXY;
     public GameObject newRoomMaxXY;
+
+    private EnemyController[] enemies;
 
     // TODO: Make a player walking animation that starts once the door is open and ends upon reachign destination. 
 
     void Start() {
         cameraController = FindObjectOfType<CameraController>();
+        levelManager = FindObjectOfType<LevelManager>();
     }
 
 	public void OnTriggerEnter2D(Collider2D other)
@@ -24,10 +29,20 @@ public class DoorTrigger : MonoBehaviour {
         if (other.gameObject.tag != "Player")
             return;
         if (playerController = other.GetComponent<PlayerController>()) {
-            StartCoroutine(playerController.DisableControls(other.gameObject, 1));
+            StartCoroutine(PlayerInDoor());
+            StartCoroutine(playerController.DisableControls(other.gameObject, timeToWait));
             cameraController.minXY = newRoomMinXY;
             cameraController.maxXY = newRoomMaxXY;
             other.gameObject.transform.position = roomSpawnPoint.transform.position;
         }
+    }
+
+    IEnumerator PlayerInDoor() {
+        playerController.SetInDoor(true);
+        levelManager.ToggleEnemies(false);
+        yield return new WaitForSecondsRealtime(timeToWait);
+        levelManager.ToggleEnemies(true);
+        playerController.SetInDoor(false);
+
     }
 }
