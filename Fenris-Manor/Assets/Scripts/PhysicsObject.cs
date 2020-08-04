92f5da677cdd8798cbb7db2278d430fea940de24
2dLynxGames <2dLynxGames@gmail.com>
2020-08-03 22:32:27 -0400

    Made box collider an Inspector item Changed box collider name Added obj requirements to top of doc

--- Fenris-Manor/Assets/Scripts/PhysicsObject.cs
+++ Fenris-Manor/Assets/Scripts/PhysicsObject.cs
@@ -2,6 +2,16 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+/*
+    * Each object to be affected by collision requires the following:
+    *      Rigidbody2D to apply physics changes to the actor
+    *      Rigidbody2D gravity to be a non 0 value (if the unit should fall due to gravity)
+    *      BoxCollider2D for grounded calculations
+    *          should be a child with the foot layer, to avoid collisions with other actors
+    *          should be small and only ground adjacent
+    *          must be attached to script in inspector
+*/
+
 public class PhysicsObject : MonoBehaviour {
     protected const float minMoveDistance = 1f / 16f;
     
@@ -10,17 +20,16 @@ public class PhysicsObject : MonoBehaviour {
     protected Vector2 targetVelocity;
 
     protected Rigidbody2D rb2d;
-    protected BoxCollider2D boxCollider2D;
+    public BoxCollider2D actorFeetCollider;
     protected Vector2 velocity;
     [SerializeField]
     protected LayerMask layerMask;
 
     protected bool isGrounded;
 
+
     protected virtual void Awake() {
-        Debug.Log("Awake is run");
         rb2d = GetComponent<Rigidbody2D>();
-        boxCollider2D = GetComponent<BoxCollider2D>();
     }
 
     void Update() {
@@ -62,7 +71,7 @@ public class PhysicsObject : MonoBehaviour {
     protected bool ObjectIsGrounded() {
         float shellDistance = 0.1f;
         if (rb2d.velocity.y == 0) {
-            RaycastHit2D raycastHit = Physics2D.BoxCast(boxCollider2D.bounds.center, boxCollider2D.bounds.size, 0f, Vector2.down, shellDistance, layerMask);
+            RaycastHit2D raycastHit = Physics2D.BoxCast(actorFeetCollider.bounds.center, actorFeetCollider.bounds.size, 0f, Vector2.down, shellDistance, layerMask);
             return raycastHit;
         }
         return false;
