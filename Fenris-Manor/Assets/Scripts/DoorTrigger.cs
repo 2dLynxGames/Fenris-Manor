a72c4971742a64d84a8e89ff2dcff43fc303a764
2dLynxGames <2dLynxGames@gmail.com>
2020-08-03 23:14:57 -0400

    Updated trigger to use parent

--- Fenris-Manor/Assets/Scripts/DoorTrigger.cs
+++ Fenris-Manor/Assets/Scripts/DoorTrigger.cs
@@ -28,12 +28,13 @@ public class DoorTrigger : MonoBehaviour {
     {
         if (other.gameObject.tag != "Player")
             return;
-        if (playerController = other.GetComponent<PlayerController>()) {
+        if (playerController = other.GetComponentInParent<PlayerController>()) {
             StartCoroutine(PlayerInDoor());
             StartCoroutine(playerController.DisableControls(other.gameObject, timeToWait));
             cameraController.minXY = newRoomMinXY;
             cameraController.maxXY = newRoomMaxXY;
-            other.gameObject.transform.position = roomSpawnPoint.transform.position;
+            Debug.Log("moving player from " + other.gameObject.transform.position + " to " + roomSpawnPoint.transform.position);
+            other.transform.parent.transform.position = roomSpawnPoint.transform.position;
         }
     }
 
