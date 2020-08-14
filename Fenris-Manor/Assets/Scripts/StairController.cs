564a4c3fa318f85677bfcc3a8a6fde7d9e1eb8d5
2dLynxGames <2dLynxGames@gmail.com>
2020-08-14 00:08:25 -0400

    Removed Edge Collider stuff

--- Fenris-Manor/Assets/Scripts/StairController.cs
+++ Fenris-Manor/Assets/Scripts/StairController.cs
@@ -5,16 +5,16 @@ using UnityEngine;
 public class StairController : MonoBehaviour
 {
     public enum STAIR_DIRECTION {
-        Up,
-        Down,
+        up,
+        down,
         none
     };
 
     private int numSteps = 0;
 
     protected STAIR_DIRECTION stairDirection;
-    private EdgeCollider2D edgeCollider;
-    private List<Vector2> verticies = new List<Vector2>();
+    //private EdgeCollider2D edgeCollider;
+    //private List<Vector2> verticies = new List<Vector2>();
 
     public GameObject leftEndStep;
     public GameObject rightEndStep;
@@ -22,62 +22,62 @@ public class StairController : MonoBehaviour
     public float timer;
 
     void Awake() {
-        
-        edgeCollider = GetComponent<EdgeCollider2D>();
-        numSteps = CalculateNumSteps();
         stairDirection = DetermineStairDirection();
-        if (stairDirection == STAIR_DIRECTION.Up) {
-            // create the bottom of the stair step for the line y = x
-            for (int x = 0; x <= ((numSteps - 1) / 2); x++) {
-                verticies.Add( new Vector2(x, x));
-                verticies.Add( new Vector2(x, x + 0.5f));
-                verticies.Add( new Vector2(x + 0.5f, x + 0.5f));
-                verticies.Add( new Vector2(x + 0.5f, x + 1f));
-                if (x == ((numSteps - 1) / 2)) {
-                    verticies.Add( new Vector2(x + 1f, x + 1f));
-                    verticies.Add( new Vector2(x + 1f, x + 1.5f));
-                }
-            }
-            // create the top half of the stair step for the line y = x
-            for (int x = ((numSteps - 1) / 2); x >= 0; x--) {
-                verticies.Add( new Vector2(x + 0.5f, x + 1.5f));
-                verticies.Add( new Vector2(x + 0.5f, x + 1f));
-                verticies.Add( new Vector2(x, x + 1f));
-                verticies.Add( new Vector2(x, x + 0.5f));
-                if (x == 0) {
-                    verticies.Add(new Vector2(0,0));
-                }
-            }
-        } if (stairDirection == STAIR_DIRECTION.Down) {
-            // same as above for y = -x
-            for (int x = 0; x <= ((numSteps - 1) / 2); x++) {
-                verticies.Add( new Vector2(-x, x));
-                verticies.Add( new Vector2(-x, x + 0.5f));
-                verticies.Add( new Vector2(-x - 0.5f, x + 0.5f));
-                verticies.Add( new Vector2(-x - 0.5f, x + 1f));
-                if (x == ((numSteps - 1) / 2)) {
-                    verticies.Add( new Vector2(-x - 1f, x + 1f));
-                    verticies.Add( new Vector2(-x - 1f, x + 1.5f));
-                }
-            }
-            for (int x = ((numSteps - 1) / 2); x >= 0; x--) {
-                verticies.Add( new Vector2(-x - 0.5f, x + 1.5f));
-                verticies.Add( new Vector2(-x - 0.5f, x + 1f));
-                verticies.Add( new Vector2(-x, x + 1f));
-                verticies.Add( new Vector2(-x, x + 0.5f));
-                if (x == 0) {
-                    verticies.Add( new Vector2(0, 0));
-                }
-            }
-        }
-        SetPoints();
+        // All this stuff is for jumping onto stairs, which we will investigate later.
+        //edgeCollider = GetComponent<EdgeCollider2D>();
+        //numSteps = CalculateNumSteps();
+        // if (stairDirection == STAIR_DIRECTION.Up) {
+        //     // create the bottom of the stair step for the line y = x
+        //     for (int x = 0; x <= ((numSteps - 1) / 2); x++) {
+        //         verticies.Add( new Vector2(x, x));
+        //         verticies.Add( new Vector2(x, x + 0.5f));
+        //         verticies.Add( new Vector2(x + 0.5f, x + 0.5f));
+        //         verticies.Add( new Vector2(x + 0.5f, x + 1f));
+        //         if (x == ((numSteps - 1) / 2)) {
+        //             verticies.Add( new Vector2(x + 1f, x + 1f));
+        //             verticies.Add( new Vector2(x + 1f, x + 1.5f));
+        //         }
+        //     }
+        //     // create the top half of the stair step for the line y = x
+        //     for (int x = ((numSteps - 1) / 2); x >= 0; x--) {
+        //         verticies.Add( new Vector2(x + 0.5f, x + 1.5f));
+        //         verticies.Add( new Vector2(x + 0.5f, x + 1f));
+        //         verticies.Add( new Vector2(x, x + 1f));
+        //         verticies.Add( new Vector2(x, x + 0.5f));
+        //         if (x == 0) {
+        //             verticies.Add(new Vector2(0,0));
+        //         }
+        //     }
+        // } if (stairDirection == STAIR_DIRECTION.Down) {
+        //     // same as above for y = -x
+        //     for (int x = 0; x <= ((numSteps - 1) / 2); x++) {
+        //         verticies.Add( new Vector2(-x, x));
+        //         verticies.Add( new Vector2(-x, x + 0.5f));
+        //         verticies.Add( new Vector2(-x - 0.5f, x + 0.5f));
+        //         verticies.Add( new Vector2(-x - 0.5f, x + 1f));
+        //         if (x == ((numSteps - 1) / 2)) {
+        //             verticies.Add( new Vector2(-x - 1f, x + 1f));
+        //             verticies.Add( new Vector2(-x - 1f, x + 1.5f));
+        //         }
+        //     }
+        //     for (int x = ((numSteps - 1) / 2); x >= 0; x--) {
+        //         verticies.Add( new Vector2(-x - 0.5f, x + 1.5f));
+        //         verticies.Add( new Vector2(-x - 0.5f, x + 1f));
+        //         verticies.Add( new Vector2(-x, x + 1f));
+        //         verticies.Add( new Vector2(-x, x + 0.5f));
+        //         if (x == 0) {
+        //             verticies.Add( new Vector2(0, 0));
+        //         }
+        //     }
+        // }
+        // SetPoints();
     }
 
-    void SetPoints() {
-        edgeCollider.points = verticies.ToArray();
-    }
+    // void SetPoints() {
+    //     edgeCollider.points = verticies.ToArray();
+    // }
 
-    public STAIR_DIRECTION getStairDirection() {
+    public STAIR_DIRECTION GetStairDirection() {
         return stairDirection;
     }
 
@@ -92,10 +92,10 @@ public class StairController : MonoBehaviour
     STAIR_DIRECTION DetermineStairDirection() {
         if (leftEndStep.transform.position.y < rightEndStep.transform.position.y)
         {
-            return STAIR_DIRECTION.Up; 
+            return STAIR_DIRECTION.up; 
         } else if (leftEndStep.transform.position.y > rightEndStep.transform.position.y)
         {
-            return STAIR_DIRECTION.Down;
+            return STAIR_DIRECTION.down;
         }
         Debug.Log("Stairs in impossible state, y postion of both ends is equal. GameObject: " + this.name);
         return STAIR_DIRECTION.none;
