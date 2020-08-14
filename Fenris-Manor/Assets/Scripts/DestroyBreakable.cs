5398db77e9c4cf456ee3fcb4d2325e8168b85539
2dLynxGames <2dLynxGames@gmail.com>
2020-08-13 23:57:00 -0400

    Initial Commit

---
+++ Fenris-Manor/Assets/Scripts/DestroyBreakable.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyBreakable : MonoBehaviour
+{
+    private bool isBroken;
+
+    public void DestroyObject() {
+        if (!isBroken) {
+            isBroken = true;
+            Destroy(this.gameObject);
+        }
+    }
+}
