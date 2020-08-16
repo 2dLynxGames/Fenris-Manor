fa5901b521914910d53c25e7a04c71081a1df7f2
2dLynxGames <2dLynxGames@gmail.com>
2020-08-15 21:40:52 -0400

    Restructured data folder

--- Fenris-Manor/Assets/Data/DataScripts/Items/DroppableItemData.cs
+++ Fenris-Manor/Assets/Data/DataScripts/Items/DroppableItemData.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [CreateAssetMenuAttribute(fileName = "DroppableItem", menuName = "Scriptables/Items/Generic", order=-900)]
-public class DroppableItem : ScriptableObject
+public class DroppableItemData : ScriptableObject
 {
     public string dropName;
     public int weight;
--- Fenris-Manor/Assets/Data/DataScripts/Items/PointDropData.cs
+++ Fenris-Manor/Assets/Data/DataScripts/Items/PointDropData.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [CreateAssetMenuAttribute(fileName = "PointGem", menuName = "Scriptables/Items/Gems", order=-899)]
-public class PointDrop : DroppableItem
+public class PointDropData : DroppableItemData
 {
     public int points;
 }
