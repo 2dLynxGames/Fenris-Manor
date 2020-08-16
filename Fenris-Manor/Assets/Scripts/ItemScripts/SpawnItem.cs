b0e50422bdc29dc9918c48e2f7e66bc160e9a889
2dLynxGames <2dLynxGames@gmail.com>
2020-08-16 03:44:06 -0400

    Crated SpawnItem abstract and Points concrete These will be attached to brekables / enemies Denotes the item they should drop

---
+++ Fenris-Manor/Assets/Scripts/ItemScripts/SpawnItem.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class SpawnItem : MonoBehaviour
+{
+    protected SpriteRenderer spriteRenderer;
+
+    public virtual void ItemSpawn() {}
+}
---
+++ Fenris-Manor/Assets/Scripts/ItemScripts/SpawnPoints.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnPoints : SpawnItem
+{
+    protected int points;
+    public PointDropData pointData;
+
+    void Awake()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        spriteRenderer.sprite = pointData.sprite;
+        points = pointData.points;
+    }
+}
