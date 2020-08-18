18a4948744d0d946ba064b492acf35b12d65f76a
2dLynxGames <2dLynxGames@gmail.com>
2020-08-18 06:50:09 -0400

    Projectile Data Initial Commit

---
+++ Fenris-Manor/Assets/Data/DataScripts/ProjectileData.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenuAttribute(fileName = "ProjectileData", menuName = "Scriptables/Enemies/Projectile", order=-997)]
+public class ProjectileData : EnemyData
+{
+
+}
+
