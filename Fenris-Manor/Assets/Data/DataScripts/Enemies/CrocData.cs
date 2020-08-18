43af4c87d6ca3e886b89e664abb07954c45d5768
2dLynxGames <2dLynxGames@gmail.com>
2020-08-17 21:22:10 -0400

    Initial Commit of Croc stuffs

---
+++ Fenris-Manor/Assets/Data/DataScripts/Enemies/CrocData.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenuAttribute(fileName = "CrocData", menuName = "Scriptables/Enemies/Wolf", order=-997)]
+public class CrocData : EnemyData
+{
+    public float jumpStrength;
+    public float wakeDistance;
+    public float projectileSpeed;
+    public GameObject fireball;
+}
---
+++ Fenris-Manor/Assets/Scripts/EnemyScripts/CrocController.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CrocController : EnemyController
+{
+    public CrocData crocData;
+
+    public bool hasFired = false;
+
+    protected override void Awake() {
+        base.Awake();
+
+        enemyData = crocData;
+    }
+
+    void Start() {
+        SetMovement();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isAwake) {
+            if (isGrounded && !hasFired) {
+                StartCoroutine(ShootFireball());
+            }
+        } else {
+            WakeEnemy(crocData.wakeDistance);
+        }
+    }
+
+    IEnumerator ShootFireball() {
+        hasFired = true;
+        var fireball = Instantiate(crocData.fireball);
+        MoveProjectileLinearly(fireball);
+
+        yield return new WaitForSecondsRealtime(Random.Range(1f, 2f));
+
+        hasFired = true;
+    }
+
+    void MoveProjectileLinearly(GameObject projectile) {
+        if (moveDirection == MOVE_DIRECTION.left) {
+            rb2d.AddForce(Vector2.left * crocData.projectileSpeed);
+        } else {
+            rb2d.AddForce(Vector2.right * crocData.projectileSpeed);
+        }
+    }
+
+}
