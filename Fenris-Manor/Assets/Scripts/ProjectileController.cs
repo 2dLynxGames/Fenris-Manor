eaf5a857f50adece6672878678a541df63995766
2dLynxGames <2dLynxGames@gmail.com>
2020-08-18 06:50:23 -0400

    Probably unneeded projectile controller

---
+++ Fenris-Manor/Assets/Scripts/ProjectileController.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProjectileController : EnemyController
+{
+    public ProjectileData projectileData;
+
+    protected override void Awake() {
+        base.Awake();
+
+        enemyData = projectileData;
+    }
+}
