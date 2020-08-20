a19ff504489f394b029f55ab6b253c32b4f69ef4
2dLynxGames <2dLynxGames@gmail.com>
2020-08-20 01:32:12 -0400

    Initial Commit

---
+++ Fenris-Manor/Assets/Scripts/ProjectileScripts/ProjectileDamagePlayer.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProjectileDamagePlayer : MonoBehaviour
+{
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Player") {
+            this.GetComponentInParent<ProjectileController>().DealDamage();
+            Destroy(this.transform.parent.gameObject);
+        }
+    }
+}
